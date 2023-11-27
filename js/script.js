2264fdc1e3ca09c2db99c85422e3ef52e7cb7d7d
npcwalison <gwalisonls@gmail.com>
2023-11-27 10:42:38 -0300

    food

--- js/script.js
+++ js/script.js
@@ -3,12 +3,43 @@ const ctx = canvas.getContext("2d");
 
 const size = 30;
 
-const snake = [
-    { x: 200, y: 200 },
-    { x: 230, y: 200 }
-]
+const snake = [{ x: 270, y: 240 }]
+
+const ramdomNumber = (max, min) => {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+const ramdomPosition = () => {
+    const number = ramdomNumber(0 , canvas.width - size)
+    return Math.round(number / 30) * 30;
+}
+
+const ramdomColor = () => {
+    const red = ramdomNumber(0, 255)
+    const green = ramdomNumber(0, 255)
+    const blue = ramdomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+const food = {
+    x: ramdomPosition(),
+    y: ramdomPosition(),
+    color: ramdomColor()
+}
 
 let direction, loopId
+
+const drawFood = () => {
+
+    const {x, y, color} = food
+
+    ctx.shadowColor = color;
+    ctx.shadowBlur = 6;
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, size, size);
+    ctx.shadowBlur = 0;
+}
 
 const drawSnake = () => {
     ctx.fillStyle = "#DDD"
@@ -47,11 +78,31 @@ const moveSnake = () => {
     snake.shift()
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#191919";
+
+    for(let i = 30; i < canvas.width; i += 30) {
+        //column
+        ctx.beginPath()
+        ctx.lineTo(i, 0);
+        ctx.lineTo(i, 600);
+        ctx.stroke();
+        //rows
+        ctx.beginPath()
+        ctx.lineTo(0, i);
+        ctx.lineTo(600, i);
+        ctx.stroke();
+    }
+}
+
 const gameLoop = () => {
     //limpa o loop para que não haja bugs
     clearInterval(loopId)
     //limpa a tela de pixel a pixel.
     ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
     drawSnake()
     moveSnake()
 
@@ -64,19 +115,19 @@ gameLoop()
 
 document.addEventListener("keydown", ({ key }) => {
 
-    if(key === "ArrowRight") {
+    if(key === "ArrowRight" && key !== "ArrowLeft") {
         direction = "right";
     }
 
-    if(key === "ArrowLeft") {
+    if(key === "ArrowLeft" && key !== "ArrowRight") {
         direction = "left";
     }
 
-    if(key === "ArrowUp") {
+    if(key === "ArrowUp" && key !== "ArrowDown") {
         direction = "up";
     }
 
-    if(key === "ArrowDown") {
+    if(key === "ArrowDown" && key !== "ArrowUp") {
         direction = "down";
     }
 })
